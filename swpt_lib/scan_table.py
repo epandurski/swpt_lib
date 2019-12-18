61ae8e9ae590830e8663eab9c00f3aaacb3f93f7
Evgeni Pandurski <epandurski@gmail.com>
2019-12-18 19:38:32 +0200

    Refactoring, fixes

--- swpt_lib/scan_table.py
+++ swpt_lib/scan_table.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple, List
+from typing import List, Optional
 from collections import deque
 import sqlalchemy
 from sqlalchemy.schema import Table
@@ -11,6 +11,9 @@ import random
 
 __all__ = ['TableScanner', 'DEFAULT_BLOCKS_PER_QUERY', 'DEFAULT_TARGET_BEAT_DURATION']
 
+TD_ZERO = timedelta(seconds=0)
+TD_MIN_SLEEPTIME = timedelta(milliseconds=10)
+
 DEFAULT_BLOCKS_PER_QUERY = 40
 """The default number of blocks to be retrieved with one query."""
 
@@ -18,12 +21,11 @@ DEFAULT_TARGET_BEAT_DURATION = 25
 """The default target duration of scanning beats in milliseconds."""
 
 
-class EndOfTableError(Exception):
-    """The end of the table has been reached."""
-
-
 class TableReader:
     """Reads a table sequentially, ad infinitum."""
+
+    class EndOfTableError(Exception):
+        """The end of the table has been reached."""
 
     LAST_BLOCK_QUERY = """SELECT pg_relation_size('{tablename}') / current_setting('block_size')::int"""
 
@@ -43,7 +45,7 @@ class TableReader:
         if self.current_block < 0:
             self.current_block = random.randrange(total_blocks)
         if self.current_block >= total_blocks:
-            raise EndOfTableError()
+            raise self.EndOfTableError()
 
     def _advance_current_block(self) -> list:
         self._ensure_valid_current_block()
@@ -62,22 +64,48 @@ class TableReader:
     def read_rows(self, count) -> list:
         """Return a list of at most `count` rows."""
 
-        rows = self.queue
-        while len(rows) < count:
+        rows = []
+        while len(self.queue) < count:
             try:
-                rows.extend(self._advance_current_block())
-            except EndOfTableError:
+                self.queue.extend(self._advance_current_block())
+            except self.EndOfTableError:
                 self.current_block = 0
                 break
-        return [rows.pop() for _ in range(count)]
+        for _ in range(count):
+            try:
+                rows.append(self.queue.pop())
+            except IndexError:
+                break
+        return rows
 
 
-class RhythmParams(NamedTuple):
+class Rhythm:
+    """A helper class to maintain a constant scanning rhythm."""
+
     rows_per_beat: int
     beat_duration: timedelta
     last_beat_ended_at: datetime
-    reset_at: datetime
-    saved_time: timedelta
+    rhythm_ends_at: datetime
+    extra_time: timedelta
+
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    def _register_elapsed_time(self) -> timedelta:
+        current_ts = datetime.now(tz=timezone.utc)
+        elapsed_time = current_ts - self.last_beat_ended_at
+        self.last_beat_ended_at = current_ts
+        return elapsed_time
+
+    def register_beat(self):
+        self.extra_time += self.beat_duration - self._register_elapsed_time()
+        if self.extra_time > TD_MIN_SLEEPTIME:
+            time.sleep(self.extra_time.total_seconds())
+            self.extra_time -= self._register_elapsed_time()
+
+    @property
+    def has_ended(self):
+        return self.last_beat_ended_at >= self.rhythm_ends_at
 
 
 class TableScanner:
@@ -86,23 +114,24 @@ class TableScanner:
 
       `table`
 
-         The :class:`~sqlalchemy.schema.Table` that will be scanned
-         (`model.__table__` if declarative base is used). **Must be
-         defined in the subclass.**
+         The :class:`sqlalchemy.schema.Table` that will be scanned
+         (`model.__table__` if declarative base is used).
+
+         **Must be defined in the subclass.**
 
       `columns`
 
          An optional list of
-         :class:`~sqlalchemy.sql.expression.ColumnElement` instances
+         :class:`sqlalchemy.sql.expression.ColumnElement` instances
          to be be retrieved for each row. Most of the time it will be
          a list of :class:`~sqlalchemy.schema.Column`
          instances. Defaults to `table.columns`.
 
       `blocks_per_query`
 
-         The number of database pages (blocks) to be retrieved with
-         one query. Defaults to ``DEFAULT_BLOCKS_PER_QUERY``. It might
-         be a good idea to increase this number when the size of the
+         The number of database pages (blocks) to be retrieved per
+         query. Defaults to ``DEFAULT_BLOCKS_PER_QUERY``. It might be
+         a good idea to increase this number when the size of the
          table row is big.
 
       `target_beat_duration`
@@ -110,17 +139,11 @@ class TableScanner:
          The scanning of the table is done in a sequence of
          "beats". This attribute determines the ideal duration in
          milliseconds of those beats. The value should be big enough
-         so that, on average, the operations performed on table's rows
-         could be completed within this interval. Setting this value
-         too high may have the effect of too many rows being processed
-         simultaneously in one beat. The default value is
+         so that, on average, all the operations performed on table's
+         rows could be completed within this interval. Setting this
+         value too high may have the effect of too many rows being
+         processed simultaneously in one beat. The default value is
          ``DEFAULT_TARGET_BEAT_DURATION``.
-
-    :param engine: SQLAlchemy engine
-
-    :param completion_goal: The time interval in which the whole table
-      should be processed. This is merely an approximate goal. In
-      reality, scans can take any amount of time.
 
     Exapmle::
 
@@ -137,57 +160,31 @@ class TableScanner:
 
     """
 
-    TD_ZERO = timedelta(seconds=0)
-    TD_MIN_SLEEPTIME = timedelta(milliseconds=10)
     TOTAL_ROWS_QUERY = """SELECT reltuples::bigint FROM pg_catalog.pg_class WHERE relname = '{tablename}'"""
 
-    columns = None
-    blocks_per_query = DEFAULT_BLOCKS_PER_QUERY
-    target_beat_duration = DEFAULT_TARGET_BEAT_DURATION
+    table: Connectable = None
+    columns: Optional[List[ColumnElement]] = None
+    blocks_per_query: int = DEFAULT_BLOCKS_PER_QUERY
+    target_beat_duration: int = DEFAULT_TARGET_BEAT_DURATION
 
-    def __init__(self, engine: Connectable, completion_goal: timedelta):
-        assert hasattr(self, 'table'), '"table" must be defined in the subclass.'
-        self.__engine = engine
-        self.__completion_goal = completion_goal
-        self.__reader = TableReader(engine, self.table, self.blocks_per_query, self.columns)
-        self.__rhythm = None
-
-    def __set_rhythm(self) -> None:
-        completion_goal = self.__completion_goal
-        assert completion_goal > self.TD_ZERO
-        target_beat_duration = timedelta(milliseconds=self.target_beat_duration)
-        assert target_beat_duration > self.TD_ZERO
-        target_number_of_beats = max(1, completion_goal // target_beat_duration)
-        total_rows = self.__engine.execute(self.TOTAL_ROWS_QUERY.format(tablename=self.table.name)).scalar()
+    def __calc_rhythm(self, total_rows: int, completion_goal: timedelta) -> Rhythm:
         assert total_rows >= 0
+        assert completion_goal > TD_ZERO
+        assert self.target_beat_duration > 0
+        target_beat_duration = timedelta(milliseconds=self.target_beat_duration)
+        target_number_of_beats = max(1, completion_goal // target_beat_duration)
         rows_per_beat = max(1, ceil(total_rows / target_number_of_beats))
         number_of_beats = max(1, ceil(total_rows / rows_per_beat))
         current_ts = datetime.now(tz=timezone.utc)
-        self.__rhythm = RhythmParams(
+        return Rhythm(
             rows_per_beat=rows_per_beat,
             beat_duration=completion_goal / number_of_beats,
             last_beat_ended_at=current_ts,
-            reset_at=current_ts + completion_goal,
-            saved_time=self.TD_ZERO,
+            rhythm_ends_at=current_ts + completion_goal,
+            extra_time=TD_ZERO,
         )
 
-    def __calc_elapsed_time(self) -> timedelta:
-        rhythm = self.__rhythm
-        current_ts = datetime.now(tz=timezone.utc)
-        elapsed_time = current_ts - rhythm.last_beat_ended_at
-        rhythm.last_beat_ended_at = current_ts
-        return elapsed_time
-
-    def __beat(self) -> None:
-        rhythm = self.__rhythm
-        rows = self.__reader.read_rows(count=rhythm.rows_per_beat)
-        self.process_rows(rows)
-        rhythm.saved_time += rhythm.beat_duration - self.__calc_elapsed_time()
-        if rhythm.saved_time > self.TD_MIN_SLEEPTIME:
-            time.sleep(rhythm.saved_time.total_seconds())
-            rhythm.saved_time -= self.__calc_elapsed_time()
-
-    def run(self):
+    def run(self, engine: Connectable, completion_goal: timedelta):
         """Scan table continuously.
 
         The table is scanned sequentially, starting from a random
@@ -195,20 +192,30 @@ class TableScanner:
         invoked with a list of rows. When the end of the table is
         reached, the scan continues from the beginning, ad infinitum.
 
+        :param engine: SQLAlchemy engine
+
+        :param completion_goal: The time interval in which the whole table
+          should be processed. This is merely an approximate goal. In
+          reality, scans can take any amount of time.
+
         """
 
-        rhythm = self.__rhythm
+        assert self.table is not None, '"table" must be defined in the subclass.'
+        reader = TableReader(engine, self.table, self.blocks_per_query, self.columns)
         while True:
-            self.__set_rhythm()
-            while rhythm.last_beat_ended_at < rhythm.reset_at:
-                self.__beat()
+            total_rows = engine.execute(self.TOTAL_ROWS_QUERY.format(tablename=self.table.name)).scalar()
+            rhythm = self.__calc_rhythm(total_rows, completion_goal)
+            while not rhythm.has_ended:
+                rows = reader.read_rows(count=rhythm.rows_per_beat)
+                self.process_rows(rows)
+                rhythm.register_beat()
 
     def process_rows(self, rows: list) -> None:
         """Process a list or rows.
 
         **Must be defined in the subclass.**
 
-        :param rows: A list of table rows
+        :param rows: A list of table rows. It could be an empty list.
 
         """
 
